551f4ced585575f629e1ff7eec754a887a6455f7
Girish Hari <bunnefant@gmail.com>
2021-02-21 03:42:32 -0500

    added more to monitor screen

---
+++ dolby/app/src/main/java/com/example/dolby_test/call.java
@@ -0,0 +1,14 @@
+package com.example.dolby_test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class call extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_call);
+    }
+}
