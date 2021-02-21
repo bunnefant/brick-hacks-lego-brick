c414e0b5f8c8802147023ab2ecbca1d6021025b4
Sohil Kollipara <>
2021-02-21 07:31:30 -0500

    Get Startup Working

--- dolby/app/src/main/java/com/example/dolby_test/OnboardingActivity.java
+++ dolby/app/src/main/java/com/example/dolby_test/OnboardingActivity.java
@@ -2,6 +2,8 @@ package com.example.dolby_test;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
@@ -16,16 +18,29 @@ public class OnboardingActivity extends AppCompatActivity {
     public ViewPager viewpager;
     Adapter_walkthrough adapter_walkthrough;
 
-    @OnClick(R.id.tv_getstarted)
+   @OnClick(R.id.next)
     public void onLeave()   {
         Intent intent = new Intent(this, record.class);
         startActivity(intent);
-    }
+       android.util.Log.e("myApp", "on leave ran");
 
+   }
+    public void startNewActivity() {
+        Intent intent = new Intent(this, record.class);
+        startActivity(intent);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onboarding);
+        Button button = (Button) findViewById(R.id.next);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startNewActivity();
+                android.util.Log.e("myApp", "on click ran");
+            }
+        });
 
         viewpager = findViewById(R.id.viewpager);
 
