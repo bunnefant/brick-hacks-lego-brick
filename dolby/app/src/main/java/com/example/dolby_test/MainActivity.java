c061dab159ef827175537cbe0e2dcfc29a8d014a
Girish Hari <bunnefant@gmail.com>
2021-02-21 03:55:25 -0500

    created record view

--- dolby/app/src/main/java/com/example/dolby_test/MainActivity.java
+++ dolby/app/src/main/java/com/example/dolby_test/MainActivity.java
@@ -45,10 +45,8 @@ import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final Integer RecordAudioRequestCode = 1;
-    private SpeechRecognizer speechRecognizer;
-    private EditText editText;
-    private ImageView micButton;
+
+
     protected List<View> views = new ArrayList<>();
     protected List<View> buttonsNotLoggedIn = new ArrayList<>();
     protected List<View> buttonsInConference = new ArrayList<>();
@@ -61,22 +59,16 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-    @Bind(R.id.user_name)
-    EditText user_name;
+
 
 
 
     @OnClick(R.id.startButton)
     public void startAppTest()  {
-        Intent intent = new Intent(this, monitor.class);
-        intent.putExtra("Name", user_name.getText().toString());
+        Intent intent = new Intent(this, record.class);
         startActivity(intent);
     }
-    private void checkPermission() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.RECORD_AUDIO},RecordAudioRequestCode);
-        }
-    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //user_name.setHint("Name");
@@ -85,83 +77,10 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         //all the logic of the onCreate will be put after this comment
-        if(ContextCompat.checkSelfPermission(this,Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED){
-            checkPermission();
-        }
-        editText = findViewById(R.id.text);
-        micButton = findViewById(R.id.button);
-        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
-
-
-        final Intent speechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-        speechRecognizer.setRecognitionListener(
-                    new RecognitionListener() {
-                    @Override
-                    public void onReadyForSpeech(Bundle bundle) {
-
-                    }
-
-                    @Override
-                    public void onBeginningOfSpeech() {
-                        //editText.setText("");
-                        //editText.setHint("Speech to Text Started Up");
-                    }
-
-                    @Override
-                    public void onRmsChanged(float v) {
-
-                    }
-
-                    @Override
-                    public void onBufferReceived(byte[] bytes) {
-
-                    }
-
-                    @Override
-                    public void onEndOfSpeech() {
-                        //editText.setHint("Speech ended");
-                    }
-
-                    @Override
-                    public void onError(int i) {
-                        //editText.setHint("Error Code: " + i);
-                    }
-
-                    @Override
-                    public void onResults (Bundle bundle){
-                        micButton.setImageResource(R.drawable.biolabselfie);
-                        ArrayList<String> data = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
-                        editText.setText(data.get(0));
-                    }
-
-                    @Override
-                    public void onPartialResults(Bundle bundle) {
-                    }
-
-                    @Override
-                    public void onEvent(int i, Bundle bundle) {
-
-                    }
-                });
-        micButton.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                if (motionEvent.getAction() == MotionEvent.ACTION_UP){
-                    speechRecognizer.stopListening();
-                }
-                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN){
-                    micButton.setImageResource(R.drawable.biolabselfie);
-                    speechRecognizer.startListening(speechRecognizerIntent);
-                }
-                return false;
-            }
-        });
 
 
         //we now initialize the sdk
-        VoxeetSDK.initialize("grB4NiWlMEvzpaLbBKBmVw==", "ap6TnDQpnFUEPlIgrN3ir3hoL2NLrCLHLHd1s_YjYW0=");
+//        VoxeetSDK.initialize("grB4NiWlMEvzpaLbBKBmVw==", "ap6TnDQpnFUEPlIgrN3ir3hoL2NLrCLHLHd1s_YjYW0=");
 
        /* //adding the user_name, login and logout views related to the open/close and conference flow
         add(views, R.id.login);
@@ -238,10 +157,6 @@ public class MainActivity extends AppCompatActivity {
         return this;
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        speechRecognizer.destroy();
-    }
+
 
 }
